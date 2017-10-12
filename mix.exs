6cdf969fb896acd73a46fc4a971a10256dc0f6ae
Sid Ngeth <sngeth@gmail.com>
2017-10-11 23:54:48 -0400

    Add wallaby

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Todone.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:wallaby, "~> 0.19.1", only: :test}
     ]
   end
 
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
 ExUnit.start()
 
 Ecto.Adapters.SQL.Sandbox.mode(Todone.Repo, :manual)
-
+{:ok, _} = Application.ensure_all_started(:wallaby)
