d2f320a241af3bd9b2d68b999f1214e3d56e99b2
Sid Ngeth <sngeth@gmail.com>
2017-10-18 08:51:54 -0400

    Refactor todos feature tests
    - Only allow user to create 1 todo at a time
    - Changed the profile link to dashboard

--- test/features/todos_test.exs
+++ test/features/todos_test.exs
@@ -1,12 +1,20 @@
 defmodule Todone.ViewTodoTest do
   use Todone.FeatureCase, async: true
-  import Wallaby.Query, only: [css: 2, text_field: 1, button: 1]
+  import Wallaby.Query, only: [css: 2, text_field: 1, button: 1, link: 1]
   alias Todone.Factory
 
   test "Creating a todo", %{session: session} do
+    Factory.insert!(:user)
+
     session
-    |> visit("/todos/new")
-    |> assert_has(css("h2", text: "New Todo"))
+    |> visit("/")
+    |> click(link("Log in"))
+    |> assert_has(css("h2", test: "Log in"))
+    |> fill_in(text_field("Email"), with: "test@test.com")
+    |> fill_in(text_field("Password"), with: "password")
+    |> click(button("Login"))
+    |> click(link("Dashboard"))
+    |> click(link("New Todo"))
     |> fill_in(text_field("Description"), with: "Learn Elixir")
     |> click(button("Submit"))
     |> assert_has(css("h2", text: "Show Todo"))
@@ -14,8 +22,23 @@ defmodule Todone.ViewTodoTest do
     |> assert_has(css(".alert-info", text: "Todo created successfully."))
   end
 
+  test "List with existing todo", %{session: session} do
+    Factory.insert!(:user)
+    Factory.insert!(:todo, description: "Learn Elixir")
+
+    session
+    |> visit("/")
+    |> click(link("Log in"))
+    |> assert_has(css("h2", test: "Log in"))
+    |> fill_in(text_field("Email"), with: "test@test.com")
+    |> fill_in(text_field("Password"), with: "password")
+    |> click(button("Login"))
+    |> click(link("Dashboard"))
+    |> refute_has(css("h2", text: "New Todo"))
+  end
+
   test "Show a todo", %{session: session} do
-    user = Factory.insert!(:user)
+    Factory.insert!(:user)
     todo = Factory.insert!(:todo, description: "Learn Elixir")
 
     session
