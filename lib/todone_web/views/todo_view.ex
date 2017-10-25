5eff8a500f11578a0bfd61f20adc076ed9e24b4e
Sid Ngeth <sngeth@gmail.com>
2017-10-25 09:26:37 -0400

    Fix rendering todos with no categories

--- lib/todone_web/views/todo_view.ex
+++ lib/todone_web/views/todo_view.ex
@@ -1,3 +1,12 @@
 defmodule TodoneWeb.TodoView do
   use TodoneWeb, :view
+  alias Todone.Todos.Todo
+
+  def category_name(nil) do
+    "None"
+  end
+
+  def category_name(category) do
+    category.name
+  end
 end
